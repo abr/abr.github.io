ab3a8c78af671b6496840ae2aeed57dea2420def
Trevor Bekolay <tbekolay@gmail.com>
2020-05-28 08:09:05 -0500

    Run prettier to js and scss files

--- js/shopify.js
+++ js/shopify.js
@@ -1,205 +1,227 @@
 const loadScript = () => {
-  const scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-  const script = document.createElement('script');
+  const scriptURL =
+    "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
+  const script = document.createElement("script");
   script.async = true;
   script.src = scriptURL;
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+  (
+    document.getElementsByTagName("head")[0] ||
+    document.getElementsByTagName("body")[0]
+  ).appendChild(script);
   script.onload = ShopifyBuyInit;
-}
-
+};
 
 const ShopifyBuyInit = () => {
   const client = ShopifyBuy.buildClient({
-    domain: 'applied-brain-research-inc.myshopify.com',
-    storefrontAccessToken: '5d17dbfff7e6fd1c5b5f5930620874df',
+    domain: "applied-brain-research-inc.myshopify.com",
+    storefrontAccessToken: "5d17dbfff7e6fd1c5b5f5930620874df",
   });
-  ShopifyBuy.UI.onReady(client).then(function (ui) {
-    const productNodes = document.getElementsByClassName('shopify-product');
+  ShopifyBuy.UI.onReady(client).then(function(ui) {
+    const productNodes = document.getElementsByClassName("shopify-product");
 
     for (let i = 0; i < productNodes.length; i++) {
       const productNode = productNodes[i];
 
-      const productVariantNodes = document.getElementsByClassName(`shopify-product-${productNode.dataset.productid}`);
+      const productVariantNodes = document.getElementsByClassName(
+        `shopify-product-${productNode.dataset.productid}`
+      );
 
-      for(let a = 0; a < productVariantNodes.length; a++) {
+      for (let a = 0; a < productVariantNodes.length; a++) {
         const variantNode = productVariantNodes[a];
 
-        generateComponent({ui,id:productNode.dataset.productid, variantId:variantNode.dataset.variantid, node:variantNode});
+        generateComponent({
+          ui,
+          id: productNode.dataset.productid,
+          variantId: variantNode.dataset.variantid,
+          node: variantNode,
+        });
       }
 
-      const singleProductNode = document.getElementsByClassName(`shopify-single-product-${productNode.dataset.productid}`);
+      const singleProductNode = document.getElementsByClassName(
+        `shopify-single-product-${productNode.dataset.productid}`
+      );
 
-      if(singleProductNode.length > 0) {
-        generateComponent({ui,id:productNode.dataset.productid, node:singleProductNode[0]});
+      if (singleProductNode.length > 0) {
+        generateComponent({
+          ui,
+          id: productNode.dataset.productid,
+          node: singleProductNode[0],
+        });
       }
 
-      generateComponent({ui,id:productNode.dataset.productid, node:productNode, isViewProduct: true, text:productNode.dataset.productname});
+      generateComponent({
+        ui,
+        id: productNode.dataset.productid,
+        node: productNode,
+        isViewProduct: true,
+        text: productNode.dataset.productname,
+      });
     }
   });
-}
+};
 
-const generateComponent = ({ui, variantId, id, node, isViewProduct, text="Add"}) => {
-  const options = isViewProduct ? VIEWPRODUCT: ADDPRODUCT;
-  ui.createComponent('product', {
+const generateComponent = ({
+  ui,
+  variantId,
+  id,
+  node,
+  isViewProduct,
+  text = "Add",
+}) => {
+  const options = isViewProduct ? VIEWPRODUCT : ADDPRODUCT;
+  ui.createComponent("product", {
     id,
     variantId,
     node,
-    moneyFormat: '%24%7B%7Bamount%7D%7D%20USD',
+    moneyFormat: "%24%7B%7Bamount%7D%7D%20USD",
     options: {
       product: {
-        text:{button: text}
-        , ...options
+        text: { button: text },
+        ...options,
       },
-      ...GENERALOPTIONS
-    }
+      ...GENERALOPTIONS,
+    },
   });
-
-}
+};
 
 const VIEWPRODUCT = {
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "calc(25% - 20px)",
-          "margin-left": "20px",
-          "margin-bottom": "50px"
-        },
-        "text-align": "left",
+  styles: {
+    product: {
+      "@media (min-width: 601px)": {
+        "max-width": "calc(25% - 20px)",
+        "margin-left": "20px",
+        "margin-bottom": "50px",
       },
-      "buttonWrapper": {
-        "margin": "0",
-      },
-      "button": {
-        "font-family": "Roboto, sans-serif",
-        "font-weight": "bold",
-        "text-align": "left",
-        "color": "#8bb684",
-        "padding": "0",
-        ":hover": {
-          "background-color": "#ffffff"
-        },
+      "text-align": "left",
+    },
+    buttonWrapper: {
+      margin: "0",
+    },
+    button: {
+      "font-family": "Roboto, sans-serif",
+      "font-weight": "bold",
+      "text-align": "left",
+      color: "#8bb684",
+      padding: "0",
+      ":hover": {
         "background-color": "#ffffff",
-        ":focus": {
-          "background-color": "#ffffff"
-        },
-        "border-radius": "2px"
-      }
+      },
+      "background-color": "#ffffff",
+      ":focus": {
+        "background-color": "#ffffff",
+      },
+      "border-radius": "2px",
     },
-    "buttonDestination": "modal",
-    "contents": {
-      "img": false,
-      "title": false,
-      "price": false,
-      "options": false,
-    },
-    "googleFonts": [
-      "Roboto"
-    ]
+  },
+  buttonDestination: "modal",
+  contents: {
+    img: false,
+    title: false,
+    price: false,
+    options: false,
+  },
+  googleFonts: ["Roboto"],
 };
 
 const ADDPRODUCT = {
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "calc(25% - 20px)",
-          "margin-left": "20px",
-          "margin-bottom": "50px"
-        }
+  styles: {
+    product: {
+      "@media (min-width: 601px)": {
+        "max-width": "calc(25% - 20px)",
+        "margin-left": "20px",
+        "margin-bottom": "50px",
       },
-      "prices": {
-        "margin": "0 !important",
-        "float": "left",
-        "width": "60%",
-        "text-align": "right",
-        "padding": "3px 10px 3px 0",
-      },
-      "buttonWithQuantity": {
-        "margin": "0 !important",
-        "float": "left",
-        "width": "40%",
-        "text-align": "right",
-      },
-      "button": {
-        "font-family": "Roboto, sans-serif",
-        "font-weight": "bold",
-        ":hover": {
-          "background-color": "#7da477"
-        },
-        "background-color": "#8bb684",
-        ":focus": {
-          "background-color": "#7da477"
-        },
-        "border-radius": "2px",
-        "padding": "3px 10px"
-      },
-      "quantityInput": {
-          "padding-top": "3px",
-          "padding-bottom": "3px",
-      }
     },
-    "layout": "horizontal",
-    "contents": {
-      "img": false,
-      "title": false,
-      "price": true,
-      "options": false,
-      "buttonWithQuantity": true,
-      "button": false,
+    prices: {
+      margin: "0 !important",
+      float: "left",
+      width: "60%",
+      "text-align": "right",
+      padding: "3px 10px 3px 0",
     },
-    "googleFonts": [
-      "Roboto"
-    ]
+    buttonWithQuantity: {
+      margin: "0 !important",
+      float: "left",
+      width: "40%",
+      "text-align": "right",
+    },
+    button: {
+      "font-family": "Roboto, sans-serif",
+      "font-weight": "bold",
+      ":hover": {
+        "background-color": "#7da477",
+      },
+      "background-color": "#8bb684",
+      ":focus": {
+        "background-color": "#7da477",
+      },
+      "border-radius": "2px",
+      padding: "3px 10px",
+    },
+    quantityInput: {
+      "padding-top": "3px",
+      "padding-bottom": "3px",
+    },
+  },
+  layout: "horizontal",
+  contents: {
+    img: false,
+    title: false,
+    price: true,
+    options: false,
+    buttonWithQuantity: true,
+    button: false,
+  },
+  googleFonts: ["Roboto"],
 };
 
 const GENERALOPTIONS = {
-  "productSet": {
-    "styles": {
-      "products": {
+  productSet: {
+    styles: {
+      products: {
         "@media (min-width: 601px)": {
-          "margin-left": "-20px"
-        }
-      }
-    }
-  },
-  "modalProduct": {
-    "contents": {
-      "img": false,
-      "imgWithCarousel": true,
-      "button": false,
-      "buttonWithQuantity": true
+          "margin-left": "-20px",
+        },
+      },
     },
-    "styles": {
-      "product": {
+  },
+  modalProduct: {
+    contents: {
+      img: false,
+      imgWithCarousel: true,
+      button: false,
+      buttonWithQuantity: true,
+    },
+    styles: {
+      product: {
         "@media (min-width: 601px)": {
           "max-width": "100%",
           "margin-left": "0px",
-          "margin-bottom": "0px"
-        }
+          "margin-bottom": "0px",
+        },
       },
-      "button": {
+      button: {
         "font-family": "Roboto, sans-serif",
         "font-weight": "bold",
-        "color": "#ffffff",
+        color: "#ffffff",
         ":hover": {
-          "background-color": "#7da477"
+          "background-color": "#7da477",
         },
         "background-color": "#8bb684",
         ":focus": {
-          "background-color": "#7da477"
+          "background-color": "#7da477",
         },
-        "border-radius": "2px"
-      }
+        "border-radius": "2px",
+      },
     },
-    "googleFonts": [
-      "Roboto"
-    ],
-    "text": {
-      "button": "View Product"
-    }
+    googleFonts: ["Roboto"],
+    text: {
+      button: "View Product",
+    },
   },
-  "cart": {
-    "templates" :{
-      "footer": `{{^data.isEmpty}}
+  cart: {
+    templates: {
+      footer: `{{^data.isEmpty}}
       <div class="{{data.classes.cart.footer}}" data-element="cart.footer">
         {{#data.discounts}}
           <div class="{{data.classes.cart.discount}}" data-element="cart.discount">
@@ -228,47 +250,43 @@ const GENERALOPTIONS = {
         </p>
         <button class="{{data.classes.cart.button}}" id="checkout" type="button" data-element="cart.button">{{data.text.button}}</button>
       </div>
-     {{/data.isEmpty}}`
+     {{/data.isEmpty}}`,
     },
-    "styles": {
-      "button": {
+    styles: {
+      button: {
         "font-family": "Roboto, sans-serif",
         "font-weight": "bold",
         ":hover": {
-          "background-color": "#7da477"
+          "background-color": "#7da477",
         },
         "background-color": "#8bb684",
         ":focus": {
-          "background-color": "#7da477"
+          "background-color": "#7da477",
         },
-        "border-radius": "2px"
-      }
+        "border-radius": "2px",
+      },
     },
-    "text": {
-      "total": "Subtotal",
-      "notice": "Shipping and discount codes are added at checkout.",
-      "button": "Checkout"
+    text: {
+      total: "Subtotal",
+      notice: "Shipping and discount codes are added at checkout.",
+      button: "Checkout",
     },
-    "googleFonts": [
-      "Roboto"
-    ]
+    googleFonts: ["Roboto"],
   },
-  "toggle": {
-    "styles": {
-      "toggle": {
+  toggle: {
+    styles: {
+      toggle: {
         "font-family": "Roboto, sans-serif",
         "font-weight": "bold",
         "background-color": "#8bb684",
         ":hover": {
-          "background-color": "#7da477"
+          "background-color": "#7da477",
         },
         ":focus": {
-          "background-color": "#7da477"
-        }
-      }
+          "background-color": "#7da477",
+        },
+      },
     },
-    "googleFonts": [
-      "Roboto"
-    ]
-  }
+    googleFonts: ["Roboto"],
+  },
 };
